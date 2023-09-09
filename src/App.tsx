dbb31d3c7cb8c71fd239df9cf84e64f72bd98408
moch akbar maulana <moc.akbar119@gmail.com>
2023-09-09 23:20:25 +0700

    install import aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Hello from "@pages/Hello"
+
 function App() {
 
   return (
     <>
+      <Hello />
       <h1>Weather App</h1>
     </>
   );
---
+++ src/pages/Hello.tsx
@@ -0,0 +1,9 @@
+const Hello = () => {
+    return (
+        <h1>
+            Hello World From Pages Component
+        </h1>
+    )
+}
+
+export default Hello
